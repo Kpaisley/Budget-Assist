5546e57a2639e28212fc8ac58d18a44542b3593b
Kyle <Kpaisley73@gmail.com>
2023-05-16 17:18:56 -0700

    Created form that allows a user to add items to an array.

--- BudgetAssist/ClientApp/src/App.js
+++ BudgetAssist/ClientApp/src/App.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
+/*import AppRoutes from './AppRoutes'; */
+import { Counter } from './components/Counter';
+import { FetchData } from './components/FetchData';
+import { Home } from './components/Home';
 import { Layout } from './components/Layout';
 import './custom.css';
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () =>  {
 
-  render() {
+    const [items, setItems] = useState([]);
+    
+    
+
+
+
+    //ADD AN ITEM TO ITEMS ARRAY
+    function addItem(e) {
+        e.preventDefault();
+
+        const itemToAdd = {
+            name: e.target[0].value,
+            price: e.target[1].value,
+            category: e.target[2].value
+        }
+
+
+        setItems(items => [...items, itemToAdd]);
+        //ADD ITEMS TO SESSIONSTORAGE SO IT WILL SAVE ON REFRESH
+        
+    }
+  
     return (
       <Layout>
         <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
+                <Route index="true" element={<Home items={items} addItem={addItem} />} />
+                <Route path="/counter" element={ <Counter /> } />
+                <Route path="/fetch-data" element={ <FetchData /> } />
         </Routes>
       </Layout>
     );
-  }
+  
 }
+
+export default App;
--- BudgetAssist/ClientApp/src/components/Home.js
+++ BudgetAssist/ClientApp/src/components/Home.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Home.css';
 
-export class Home extends Component {
-  static displayName = Home.name;
+export const Home = (props) => {
 
-  render() {
+  
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+        <div>
+            <h1>Welcome to Budget Assist V1.0.0</h1>
+            <h3>Add items to your budget list below</h3>
+
+            <form id="add-item-form" onSubmit={(e) => props.addItem(e) }>
+                <label htmlFor="expense-name">Expense Name</label>
+                <input type="text" name="expense-name" id="expense-name" placeholder="Dog Food" autoFocus></input>
+
+                <label htmlFor="item-cost">Total Cost $</label>
+                <input type="number" step="0.01" name="item-cost" id="item-cost" placeholder="110.24"></input>
+
+                <label>Category</label>
+                <select>
+                    <option value=""></option>
+                    <option value="Entertainment">Entertainment</option>
+                    <option value="Groceries">Groceries</option>
+                    <option value="Eating Out">Eating Out</option>
+                    <option value="Bills">Bills</option>
+                    <option value="Fuel">Fuel</option>
+                    <option value="Pets">Pets</option>
+                    <option value="Savings">Savings</option>
+                    <option value="Other">Other</option>
+                </select>
+
+                <input type="submit"></input>
+            </form>
+            {props.items.length}
+        </div>
     );
-  }
+  
 }
+
